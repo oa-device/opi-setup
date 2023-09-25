36e66f61e8e5f877ab4a5e4532edba2da27c6457
Tam Nhu Tran <kaitran.ntt@gmail.com>
2023-09-25 11:05:23 -0400

    Update repo, commit 23a7faa475ce36e34fad6d7f9fb52cd3aaa9b569

--- init-scripts/unclutter.sh
+++ init-scripts/unclutter.sh
@@ -6,12 +6,4 @@ echo "========== SETTING UP HIDE CURSOR =========="
 echo "Installing unclutter..."
 sudo apt install -y unclutter
 
-# Add lines to .bashrc if they don't exist
-BASHRC="$HOME/.bashrc"
-
-# Check for 'export DISPLAY=:0.0'
-grep -qxF 'export DISPLAY=:0.0' "$BASHRC" || echo -e "\nexport DISPLAY=:0.0" >> "$BASHRC"
-# Check for 'unclutter &'
-grep -qxF 'unclutter &' "$BASHRC" || echo "unclutter &" >> "$BASHRC"
-
 echo "========== HIDE CURSOR SETUP COMPLETE =========="
