82ab86a9dc703911766a79522b438547fae09d31
Tam Nhu Tran <kaitran.ntt@gmail.com>
2023-11-28 12:28:34 -0500

    Update repo, commit 0e3da3bf823ea54b0d2606e9dbe6677118eb380e

--- util-scripts/wifi.sh
+++ util-scripts/wifi.sh
@@ -22,8 +22,14 @@ create_or_update_wifi() {
 
     # Add a new connection with the provided credentials
     if nmcli con add con-name "$con_name" ifname wlan0 type wifi ssid "$ssid"; then
-        nmcli con modify "$con_name" wifi-sec.key-mgmt wpa-psk
-        nmcli con modify "$con_name" wifi-sec.psk "$password"
+        # Set up WiFi security only if a password is provided
+        if [ -n "$password" ]; then
+            nmcli con modify "$con_name" wifi-sec.key-mgmt wpa-psk
+            nmcli con modify "$con_name" wifi-sec.psk "$password"
+        else
+            echo -e "\nWARNING: $con_name does not require a password, but may need extra interaction.\n"
+            nmcli con modify "$con_name" wifi-sec.key-mgmt none
+        fi
         nmcli con modify "$con_name" connection.autoconnect yes
         nmcli con modify "$con_name" connection.autoconnect-priority "$priority"
         echo "WiFi credentials updated for $con_name"
@@ -32,29 +38,56 @@ create_or_update_wifi() {
     fi
 }
 
+# Function to list available WiFi networks and handle user selection
+list_wifi_networks() {
+    echo -e "\nAvailable WiFi networks:"
+
+    # Capture the output of 'nmcli dev wifi' in a temporary file
+    TEMP_FILE=$(mktemp)
+    script -q -c "nmcli dev wifi" "$TEMP_FILE" > /dev/null
+
+    # Remove the first and last lines which are added by 'script' command
+    sed -i '1d;$d' "$TEMP_FILE"
+
+    # Store the original output with ANSI codes (colored output)
+    # Remove the first and last line of the original output
+    local original_output=$(sed '1d;$d' "$TEMP_FILE")
+
+    # Now remove ANSI escape codes for processing
+    sed -i 's/\x1B\[[0-9;]*[JKmsu]//g' "$TEMP_FILE"
+
+    # Extract and display the header (now the first line in the file)
+    local header=$(nmcli dev wifi | head -n 1)
+    echo -e "        $header"
+
+    # Display the list of networks with line numbers
+    echo "$original_output" | cat -n
+}
+
 # Check and setup default WiFi connection
 echo "Checking for default WiFi connection..."
 create_or_update_wifi "$DEFAULT_SSID" "$DEFAULT_PASSWORD" "$DEFAULT_CON_NAME" "$DEFAULT_CONNECTION_PRIORITY"
 
-# List available SSIDs and allow user to choose
-echo "Available WiFi networks:"
-nmcli dev wifi | awk '{print NR-1 " - " $0}' | tail -n +2
-echo "Enter the number of your WiFi network or 'n' to enter a new SSID:"
+# List available WiFi networks
+list_wifi_networks
+
+echo -e "\nEnter the number of your WiFi network or 'n' to enter a new SSID:"
 read -r choice
 
 if [[ $choice =~ ^[0-9]+$ ]]; then
-    USER_SSID=$(nmcli dev wifi | awk 'NR=='$((choice + 1))'{print $2}')
+    # Retrieve the SSID from the cleaned output, assuming SSID is the 2nd column
+    SSID_SELECTED=$(awk 'NR=='$((choice + 1))'{print $2}' "$TEMP_FILE")
 else
     echo "Enter new SSID:"
     read -r USER_SSID
 fi
 
 # Ask for the WiFi password
-read -s -p "Enter WiFi Password for $USER_SSID: " USER_PASSWORD
+read -p "Enter WiFi Password for $SSID_SELECTED: " USER_PASSWORD
 echo ""
 
 # Update WiFi with user provided details
-create_or_update_wifi "$USER_SSID" "$USER_PASSWORD" "$USER_SSID" 100
+create_or_update_wifi "$SSID_SELECTED" "$USER_PASSWORD" "$SSID_SELECTED" 100
 
 # Lower the priority of all Ethernet connections starting with "Wired connection"
 echo "========== SETTING UP ETHERNET PRIORITY =========="
