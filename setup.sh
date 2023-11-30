c7a469264ac18598d9f9b29e75a31c9d2f38c1d8
Tam Nhu Tran <kaitran.ntt@gmail.com>
2023-11-30 16:18:02 -0500

    Update repo, commit d74818c109cb3abdb376a6107b87d7bc6b8c99f8

--- setup.sh
+++ setup.sh
@@ -63,7 +63,15 @@ gsettings set org.blueman.plugins.powermanager auto-power-on false
 bluetooth_status=$(rfkill list bluetooth | grep -c "Soft blocked: yes")
 [ "$bluetooth_status" -gt 0 ] && echo "Bluetooth is off" || echo "Bluetooth is on"
 
-# TODO: Enable Remote Desktop and VNC
+# Configure the on-screen keyboard settings
+gsettings set org.gnome.desktop.a11y.applications screen-keyboard-enabled true
+
+# Configure power settings to prevent screen dimming and blanking
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 0
+
+# Keep grdctl code commented as a reference
 # grdctl rdp enable
 # grdctl rdp set-credentials orangepi orangead
 # grdctl rdp disable-view-only
@@ -73,13 +81,51 @@ bluetooth_status=$(rfkill list bluetooth | grep -c "Soft blocked: yes")
 # grdctl vnc disable-view-only
 # grdctl status --show-credentials
 
-# Configure the on-screen keyboard settings
-gsettings set org.gnome.desktop.a11y.applications screen-keyboard-enabled true
+# Check and clean for repeated keyring files
+KEYRING_DIR="/home/orangepi/.local/share/keyrings"
+print_section "CLEANING UP KEYRING FILES"
+# Remove repeated keyring files
+find "$KEYRING_DIR" -type f -name 'Default_keyring*.keyring' ! -name 'Default_keyring.keyring' -exec rm {} \;
+# Log the final state of keyring files
+echo "Current keyring files in the directory after cleanup:"
+ls "$KEYRING_DIR" | grep 'Default_keyring*.keyring'
 
-# Configure power settings to prevent screen dimming and blanking
-gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 0
+# Check and update keyring file for VNC settings
+print_section "UPDATING KEYRING FILE FOR VNC SETTINGS"
+KEYRING_FILE="$KEYRING_DIR/Default_keyring.keyring"
+if [ -f "$KEYRING_FILE" ]; then
+    # Determine whether an update is needed
+    vnc_entry=$(grep "org.gnome.RemoteDesktop.VncPassword" "$KEYRING_FILE")
+
+    if [[ -z "$vnc_entry" ]]; then
+        # If the entry does not exist, add it
+        new_index=$(( $(grep -oP "\[\K([0-9]+)(?=\])" "$KEYRING_FILE" | sort -nr | head -n1) + 1 ))
+        echo -e "\n[${new_index}]\nitem-type=0\ndisplay-name=GNOME Remote Desktop VNC password\nsecret=orangead\n\n[${new_index}:attribute0]\nname=xdg:schema\ntype=string\nvalue=org.gnome.RemoteDesktop.VncPassword" >> "$KEYRING_FILE"
+        echo "VNC entry added to the keyring file."
+    elif ! grep -q "secret=orangead" <<< "$vnc_entry"; then
+        # Update the existing entry if the secret is different
+        vnc_block=$(grep -n "\[.*\]" "$KEYRING_FILE" | grep -B1 "org.gnome.RemoteDesktop.VncPassword" | head -1 | cut -d: -f1)
+        sed -i "${vnc_block},${vnc_block_number:attribute0}/{s/secret=.*/secret=orangead/}" "$KEYRING_FILE"
+        echo "VNC entry updated in the keyring file."
+    else
+        echo "VNC entry is already up-to-date in the keyring file."
+    fi
+else
+    echo "Keyring file not found."
+fi
+
+
+# Check if gnome-remote-desktop is service running on port 5900 and start it if not
+print_section "CONFIGURING GNOME REMOTE DESKTOP SERVICE"
+systemctl --user start gnome-remote-desktop
+systemctl --user status gnome-remote-desktop
+
+# Enable VNC
+print_section "CONFIGURING VNC"
+grdctl vnc enable
+grdctl vnc set-auth-method password
+grdctl vnc disable-view-only
+grdctl status --show-credentials
 
 # Update and Upgrade system packages
 print_section "UPDATING SYSTEM"
