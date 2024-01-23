2d29c5d8554da42febab7bce6876652207ec44aa
Tam Nhu Tran <kaitran.ntt@gmail.com>
2024-01-23 14:35:31 -0500

    Update repo, commit d7bb8f437c291d2f5962fa17d663b0b55f1f5fb7

--- init-scripts/chromium.sh
+++ init-scripts/chromium.sh
@@ -3,7 +3,7 @@
 echo "========== SETTING UP CHROMIUM =========="
 
 echo "Installing Chromium browser..."
-sudo apt install -y chromium-browser
+sudo apt-get install -y chromium-browser
 echo "Setting Chromium as the default browser..."
 xdg-settings set default-web-browser chromium-browser.desktop
 echo "Chromium is now the default browser!"
