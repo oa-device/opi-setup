93eca60a2695934ee840ede0c38285cf7d490558
Tam Nhu Tran <kaitran.ntt@gmail.com>
2024-02-08 14:32:35 -0500

    Update repo, commit 3fe05a93183170abb2c4c4834e6213ed1f3ac166

--- player-config.sh
+++ player-config.sh
@@ -12,6 +12,33 @@ get_current_release() {
     fi
 }
 
+set_choice_based_on_current_release() {
+    local CURRENT_RELEASE=$1
+    case "$CURRENT_RELEASE" in
+        "prod") echo 1;;
+        "preprod") echo 2;;
+        "staging") echo 3;;
+    esac
+}
+
+handle_user_input() {
+    local CURRENT_RELEASE=$1
+    read -t 10 -p "Press Enter to continue using this release, or enter a number (1-3) to choose a different directory: " choice
+    if [[ -z "$choice" ]]; then
+        echo -e "\n\e[1;31mNo input received within 10 seconds, using the current release.\e[0m"
+        choice=$(set_choice_based_on_current_release "$CURRENT_RELEASE")
+    else
+        case "$choice" in
+            1) CURRENT_RELEASE="prod";;
+            2) CURRENT_RELEASE="preprod";;
+            3) CURRENT_RELEASE="staging";;
+            *) echo "Invalid choice. Please enter a valid choice (1-3):";;
+        esac
+    fi
+    echo "$CURRENT_RELEASE"
+    return $choice  # Return the choice value
+}
+
 prompt_for_directory_choice() {
     local CURRENT_RELEASE=$(get_current_release)
     echo "Which directory do you want to use?"
@@ -22,19 +49,10 @@ prompt_for_directory_choice() {
         echo -e "The current release is \e[1;33m$CURRENT_RELEASE\e[0m.\n"
         if [[ -z "$SSH_TTY" ]]; then
             echo -e "\e[1;31mNon-interactive shell or SSH connection detected, using the current release.\e[0m"
-            choice=""
+            choice=$(set_choice_based_on_current_release "$CURRENT_RELEASE")
         else
-            read -t 10 -p "Press Enter to continue using this release, or enter a number (1-3) to choose a different directory: " choice
-            if [[ -z "$choice" ]]; then
-                echo -e "\n\e[1;31mNo input received within 10 seconds, using the current release.\e[0m"
-            else
-                case "$choice" in
-                    1) CURRENT_RELEASE="prod";;
-                    2) CURRENT_RELEASE="preprod";;
-                    3) CURRENT_RELEASE="staging";;
-                    *) echo "Invalid choice. Please enter a valid choice (1-3):";;
-                esac
-            fi
+            CURRENT_RELEASE=$(handle_user_input "$CURRENT_RELEASE")
+            choice=$?  # Get the returned choice value
         fi
         WORKING_DIR="$ROOT_DIR/$CURRENT_RELEASE"
         return
