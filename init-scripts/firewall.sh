e99b9e483effdfc9102df9df80f4bc75b60269c7
Tam Nhu Tran <kaitran.ntt@gmail.com>
2024-07-03 17:28:02 -0400

    Update repo, commit 8d132e5138c85b0c9d9bab8683130d03b27a96eb

--- init-scripts/firewall.sh
+++ init-scripts/firewall.sh
@@ -1,56 +1,100 @@
 #!/bin/bash
 
 source "$(dirname "$(readlink -f "$0")")/../helpers.sh" || {
-    echo "Error: Could not source helpers.sh"
-    exit 1
+	echo "Error: Could not source helpers.sh"
+	exit 1
 }
 
 echo "---------- SETTING UP FIREWALL ----------"
 
-# Check if UFW is installed
-if ! command -v ufw &>/dev/null; then
-	echo "Installing UFW..."
-	sudo apt update
-	sudo apt install ufw -y
-	echo "UFW installed!"
-else
-	echo "UFW is already installed!"
-fi
-
-# Set default policies if not set
-if [[ $(sudo ufw status verbose | grep -c "Default: deny (incoming), allow (outgoing)") -eq 0 ]]; then
-	printf "y\n" | sudo ufw default deny incoming &>/dev/null
-	printf "y\n" | sudo ufw default allow outgoing &>/dev/null
-fi
-print_color "green" "Default policies set:"
-print_color "yellow" "- Incoming: denied (prevents unauthorized access)"
-print_color "yellow" "- Outgoing: allowed (ensures device can access external services)"
-
-# Check and set rules if not already set
-rules=("22/tcp" "80/tcp" "443" "41641")
-rule_explanations=("SSH access" "HTTP access" "HTTPS access" "Tailscale access")
-
-for i in "${!rules[@]}"; do
-	rule="${rules[$i]}"
-	explanation="${rule_explanations[$i]}"
-	if [[ $(sudo ufw status | grep -c "${rule}") -eq 0 ]]; then
-		printf "y\n" | sudo ufw allow $rule &>/dev/null
-		print_color "green" "Rule added: $rule \t ($explanation)"
+install_ufw() {
+	if ! command -v ufw &>/dev/null; then
+		echo "Installing UFW..."
+		sudo apt update
+		sudo apt install ufw -y
+		echo "UFW installed!"
 	else
-		print_color "yellow" "Rule existed: $rule \t ($explanation)"
+		echo "UFW is already installed!"
 	fi
-done
+}
 
-# Enable UFW if not already enabled
-if [[ $(sudo ufw status | grep -c "Status: active") -eq 0 ]]; then
-	printf "y\n" | sudo ufw enable &>/dev/null
-fi
-print_color "green" "\e[1mUFW enabled!\e[0m"
+set_default_policies() {
+	if [[ $(sudo ufw status verbose | grep -c "Default: deny (incoming), allow (outgoing)") -eq 0 ]]; then
+		printf "y\n" | sudo ufw default deny incoming &>/dev/null
+		printf "y\n" | sudo ufw default allow outgoing &>/dev/null
+	fi
+	print_color "green" "Default policies set:"
+	print_color "yellow" "- Incoming: denied (prevents unauthorized access)"
+	print_color "yellow" "- Outgoing: allowed (ensures device can access external services)"
+}
 
-# Print the current rules
-print_color "yellow" "---------- CURRENT FIREWALL RULES ----------"
-sudo ufw status verbose numbered | while IFS= read -r line; do
-	print_color "green" "${line}"
-done
+set_firewall_rules() {
+	local rules=("22/tcp" "80/tcp" "443" "41641/udp" "3478/udp")
+	local rule_explanations=("SSH access" "HTTP access" "HTTPS/WebSocket access" "Tailscale direct connection" "STUN protocol")
+
+	for i in "${!rules[@]}"; do
+		local rule="${rules[$i]}"
+		local explanation="${rule_explanations[$i]}"
+		if [[ $(sudo ufw status | grep -c "${rule}") -eq 0 ]]; then
+			printf "y\n" | sudo ufw allow "$rule" &>/dev/null
+			printf -v output "Rule added: %-15s (%s)" "$rule" "$explanation"
+			print_color "green" "$output"
+		else
+			printf -v output "Rule existed: %-15s (%s)" "$rule" "$explanation"
+			print_color "yellow" "$output"
+		fi
+	done
+}
+
+set_rate_limiting() {
+	if [[ $(sudo ufw status | grep -c "22/tcp (LIMIT)") -eq 0 ]]; then
+		printf "y\n" | sudo ufw limit 22/tcp &>/dev/null
+		print_color "green" "Rate limiting set for SSH (port 22)."
+	else
+		print_color "yellow" "Rate limiting already set for SSH (port 22)."
+	fi
+}
+
+remove_ipv6_rules() {
+	local ipv6_rules
+	ipv6_rules=$(sudo ufw status numbered | grep '(v6)' | awk -F'[][]' '{print $2}' | tac)
+
+	for rule_num in $ipv6_rules; do
+		sudo ufw --force delete "$rule_num" &>/dev/null
+	done
+
+	print_color "green" "All IPv6 rules have been removed."
+}
+
+enable_ufw() {
+	if [[ $(sudo ufw status | grep -c "Status: active") -eq 0 ]]; then
+		printf "y\n" | sudo ufw enable &>/dev/null
+		print_color "green" "\e[1mUFW enabled!\e[0m"
+	else
+		print_color "green" "UFW is already enabled!"
+	fi
+}
+
+enable_logging() {
+	sudo ufw logging on &>/dev/null
+	print_color "green" "UFW logging enabled."
+}
+
+print_current_rules() {
+	print_color "yellow" "---------- CURRENT FIREWALL RULES ----------"
+	sudo ufw status verbose | while IFS= read -r line; do
+		print_color "green" "${line}"
+	done
+}
+
+# Main Execution
+install_ufw
+set_default_policies
+set_firewall_rules
+set_rate_limiting
+remove_ipv6_rules
+enable_logging
+enable_ufw
+print_current_rules
 
 echo "---------- FIREWALL SETUP COMPLETE ----------"
--- util-scripts/wifi.sh
+++ util-scripts/wifi.sh
@@ -2,152 +2,122 @@
 
 echo "---------- SETTING UP WIFI ----------"
 
-# Check if running in a non-interactive SSH shell
 if [[ -z "$SSH_TTY" ]]; then
     echo "Running in a non-interactive SSH shell. Skipping process."
-else
-    # Default wifi settings
-    DEFAULT_SSID="orangead_wifi"
-    DEFAULT_PASSWORD="orangead_wifi"
-    DEFAULT_CON_NAME="OrangeAd_Debug_Wifi"
-    DEFAULT_CONNECTION_PRIORITY=999
+    exit 0
+fi
 
-    # Function to create or update a WiFi connection
-    create_or_update_wifi() {
-        local ssid="$1"
-        local password="$2"
-        local con_name="$3"
-        local priority="$4"
+# Default WiFi settings
+DEFAULT_SSID="orangead_wifi"
+DEFAULT_PASSWORD="orangead_wifi"
+DEFAULT_CON_NAME="OrangeAd_Debug_Wifi"
+DEFAULT_CONNECTION_PRIORITY=999
 
-        # Check if the connection already exists, if it does, delete it
-        if nmcli con show | grep -q "$con_name"; then
-            nmcli con delete "$con_name"
-        fi
+create_or_update_wifi() {
+    local ssid="$1"
+    local password="$2"
+    local con_name="$3"
+    local priority="$4"
 
-        # Add a new connection with the provided credentials
-        if [ -n "$password" ]; then
-            if nmcli con add con-name "$con_name" ifname wlan0 type wifi ssid "$ssid"; then
-                nmcli con modify "$con_name" wifi-sec.key-mgmt wpa-psk
-                nmcli con modify "$con_name" wifi-sec.psk "$password"
-                echo "WiFi credentials updated for $con_name"
-            else
-                echo "Failed to add new connection for $con_name."
-            fi
-        else
-            if nmcli --ask dev wifi connect "$con_name"; then
-                echo "WiFi credentials updated for $con_name"
-            else
-                echo "Failed to add new connection for $con_name."
-            fi
-        fi
+    # Delete existing connection if it exists
+    nmcli con show | grep -q "$con_name" && nmcli con delete "$con_name"
 
-        # Configure autoconnect and priority
-        nmcli con modify "$con_name" connection.autoconnect yes
-        nmcli con modify "$con_name" connection.autoconnect-priority "$priority"
-    }
+    # Add new connection
+    if [ -n "$password" ]; then
+        nmcli con add con-name "$con_name" ifname wlan0 type wifi ssid "$ssid"
+        nmcli con modify "$con_name" wifi-sec.key-mgmt wpa-psk
+        nmcli con modify "$con_name" wifi-sec.psk "$password"
+    else
+        nmcli --ask dev wifi connect "$con_name"
+    fi
 
-    # Function to list available WiFi networks and handle user selection
-    list_wifi_networks() {
-        echo -e "\nAvailable WiFi networks:"
+    # Configure autoconnect and priority
+    nmcli con modify "$con_name" connection.autoconnect yes
+    nmcli con modify "$con_name" connection.autoconnect-priority "$priority"
 
-        # Capture the output of 'nmcli dev wifi' in a temporary file
-        TEMP_FILE=$(mktemp)
-        script -q -c "nmcli dev wifi" "$TEMP_FILE" >/dev/null
+    echo "WiFi credentials updated for $con_name"
+}
 
-        # Remove the first and last lines which are added by 'script' command
-        sed -i '1d;$d' "$TEMP_FILE"
+list_wifi_networks() {
+    echo -e "\nAvailable WiFi networks:"
 
-        # Store the original output with ANSI codes (colored output)
-        # Remove the first and last line of the original output
-        local original_output=$(sed '1d;$d' "$TEMP_FILE")
+    TEMP_FILE=$(mktemp)
+    script -q -c "nmcli dev wifi" "$TEMP_FILE" >/dev/null
 
-        # Now remove ANSI escape codes for processing
-        sed -i 's/\x1B\[[0-9;]*[JKmsu]//g' "$TEMP_FILE"
+    sed -i '1d;$d' "$TEMP_FILE"
+    local original_output=$(sed '1d;$d' "$TEMP_FILE")
 
-        # Extract and display the header (now the first line in the file)
-        local header=$(nmcli dev wifi | head -n 1)
-        echo -e "        $header"
+    local header=$(nmcli dev wifi | head -n 1)
+    echo -e "        $header"
 
-        # Display the list of networks with line numbers
-        echo "$original_output" | cat -n
-    }
+    echo "$original_output" | cat -n
+}
 
-    # Function to extract the SSID, handling SSIDs with spaces
-    extract_ssid() {
-        local line_number="$1"
-        local ssid
+extract_ssid() {
+    local line_number="$1"
+    local ssid=$(sed -n "${line_number}p" "$TEMP_FILE")
 
-        # Extract the line corresponding to the user's choice
-        ssid=$(sed -n "${line_number}p" "$TEMP_FILE")
+    if [[ $ssid =~ [0-9A-Fa-f]{2}(:[0-9A-Fa-f]{2}){5}[[:space:]]+(.+)Infra ]]; then
+        ssid="${BASH_REMATCH[2]}"
+        ssid=$(echo "$ssid" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+    else
+        echo "Failed to extract SSID."
+        return 1
+    fi
 
-        # Use a regular expression to extract the SSID
-        # This regex assumes that the SSID is followed by 'Infra' and is after the BSSID
-        if [[ $ssid =~ [0-9A-Fa-f]{2}(:[0-9A-Fa-f]{2}){5}[[:space:]]+(.+)Infra ]]; then
-            ssid="${BASH_REMATCH[2]}"
-            # Trim leading and trailing spaces
-            ssid=$(echo "$ssid" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
-        else
-            echo "Failed to extract SSID."
-            return 1
-        fi
+    echo "$ssid"
+}
 
-        echo "$ssid"
-    }
-
-    # Check and setup default WiFi connection
+configure_default_wifi() {
     echo "Checking for default WiFi connection..."
     create_or_update_wifi "$DEFAULT_SSID" "$DEFAULT_PASSWORD" "$DEFAULT_CON_NAME" "$DEFAULT_CONNECTION_PRIORITY"
+}
 
-    # List available WiFi networks
-    list_wifi_networks
-
+configure_wifi_network() {
     echo -e "\n\e[1;31mIf no input is made within 30 seconds, the script will skip the WiFi setup.\e[0m"
     echo "Enter the number of your WiFi network, type 'n' to enter a new SSID, or hit Enter to skip:"
     read -t 30 -r choice
 
     if [[ $? -ne 0 ]]; then
         echo "Skipping WiFi setup due to timeout."
-        exit 1
+        return
     fi
 
     if [[ $choice =~ ^[0-9]+$ ]]; then
         SSID_SELECTED=$(extract_ssid $((choice + 1)))
         if [[ $? -ne 0 ]]; then
             echo "Error extracting SSID. Please try again."
-            exit 1
+            return
         fi
 
-        # Ask for the WiFi password.
         read -p "Enter WiFi Password for \"$SSID_SELECTED\": " USER_PASSWORD
         echo ""
-
-        # Update WiFi with user provided details
         create_or_update_wifi "$SSID_SELECTED" "$USER_PASSWORD" "$SSID_SELECTED" 100
     elif [[ $choice == "n" ]]; then
-        echo "Enter new SSID:"
-        read -r SSID_SELECTED
-
-        # Ask for the WiFi password.
+        read -p "Enter new SSID: " SSID_SELECTED
         read -p "Enter WiFi Password for \"$SSID_SELECTED\": " USER_PASSWORD
         echo ""
-
-        # Update WiFi with user provided details
         create_or_update_wifi "$SSID_SELECTED" "$USER_PASSWORD" "$SSID_SELECTED" 100
     else
         echo "Skipping WiFi setup."
     fi
+}
 
-    # Lower the priority of all Ethernet connections starting with "Wired connection"
+set_ethernet_priority() {
     echo "---------- SETTING UP ETHERNET PRIORITY ----------"
-
-    # Loop through each "Wired connection" and set their metric higher than WiFi
-    while IFS= read -r ETH_CON_NAME; do
-        if [[ ! -z "$ETH_CON_NAME" ]]; then
+    nmcli -t -f NAME con show | grep "^Wired connection" | while read -r ETH_CON_NAME; do
+        if [[ -n "$ETH_CON_NAME" ]]; then
             nmcli con modify "$ETH_CON_NAME" ipv4.route-metric 700
             nmcli con modify "$ETH_CON_NAME" ipv6.route-metric 700
             echo "Lowered priority for Ethernet connection: $ETH_CON_NAME"
         fi
-    done < <(nmcli -t -f NAME con show | grep "^Wired connection")
-fi
+    done
+}
+
+# Main Execution
+configure_default_wifi
+list_wifi_networks
+configure_wifi_network
+set_ethernet_priority
 
 echo "---------- WIFI SETUP COMPLETE ----------"
